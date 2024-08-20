e710205c6843f493cc9063ee2594cee61e0a6236
Kilyong Jeong <uzoolove@gmail.com>
2024-08-20 11:22:15 +0900

    👔 신청곡 목록 컴포넌트를 클라이언트 컴포넌트로 변경

--- src/app/music/[id]/RequestSongList.tsx
+++ src/app/music/[id]/RequestSongList.tsx
@@ -1,23 +1,39 @@
+'use client';
+
 import { MusicComment } from "@/types";
 import RequestSongItem from "./RequestSongItem";
 import RequestSongNew from "./RequestSongNew";
 import { fetchComments } from "@/model/fetch/postFetch";
-import { auth } from "@/auth";
+import { useEffect, useState } from "react";
 
-export default async function RequestSongList({ type, id }: { type: string, id: string }) {
-  const session = await auth();
+export default function RequestSongList({ type, id }: { type: string, id: string }) {
+  // const data = await fetchComments(id) as MusicComment[];
+  // const list = data.map(item => <RequestSongItem key={item._id} item={item} />);
+
+  const [requestSongList, setRequestSongList] = useState<MusicComment[]>([]);
+
+  const fetchData = async (postId: string) => {
+    const data = await fetchComments(postId) as MusicComment[];
+    setRequestSongList(data);
+  };
   
-  const data = await fetchComments(id) as MusicComment[];
-  const list = data.map(item => <RequestSongItem key={item._id} item={item} />);
+  useEffect(() => {
+    fetchData(id);
+  }, [id]); // id가 변경될 때만 fetchData 실행
+  
+  const handleNewSong = async () => {
+    fetchData(id);
+  };
+
+  const list = requestSongList.map(item => <RequestSongItem key={item._id} item={item} />);
+
+
   return (
     <section className="mb-8">
       <h4 className="mt-8 mb-4 ml-2">신청곡 { list?.length || 0 }개</h4>
       { list }
-      { session?.user?.id ? (
-        <RequestSongNew id={id} />
-      ) : (
-        <p>음악 신청은 로그인 후 이용 가능합니다.</p>
-      ) }
+
+      <RequestSongNew id={id} onNewSong={handleNewSong} />
     </section>
   );
 }
