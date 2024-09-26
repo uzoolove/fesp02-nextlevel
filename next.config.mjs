75b824c12e6c2b8e21511ed77e1f7a0a4949c037
Kilyong Jeong <uzoolove@gmail.com>
2024-09-26 10:43:48 +0900

    설정

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,11 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'next.fesp.shop',
+        pathname: '/files/**',
+      },
+      {
+        protocol: 'https',
         hostname: '*.googleusercontent.com',
         pathname: '**',
       },
